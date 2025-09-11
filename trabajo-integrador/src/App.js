7f01eeb046a4dc81447ed6c77aff580a53930d81
Finnet97 <totochipont@gmail.com>
2025-09-10 23:58:53 -0300

    feat: Base de sistema de rutas

--- trabajo-integrador/src/App.js
+++ trabajo-integrador/src/App.js
@@ -1,24 +1,28 @@
 // import logo from './logo.svg';
 // import './App.css';
-import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
-import Home from "./screens/Home/Home";
+
+import { Switch, Route, Redirect } from "react-router-dom";
+import Index from "./screens/Index/Index";
+import Movie from "./screens/Movie/Movie";
 import Movies from "./screens/Movies/Movies";
-import NotFound from "./screens/NotFound/NotFound";
 import Serie from "./screens/Serie/Serie";
 import Series from "./screens/Series/Series";
-import Movie from "./screens/Movie/Movie"
+import Favourites from "./screens/Favourites/Favourites";
 import Results from "./screens/Results/Results";
+import NotFound from "./screens/NotFound/NotFound";
+
 function App() {
   return (
-      <Switch>
-        <Route path="/" exact={true} component={Home}/>
-        <Route path="/Movies" exact={true} component={Movies}/>
-        <Route path="/Movie/id/:id" component={Movie}/>
-        <Route path="/Series" exact={true} component={Series}/>
-        <Route path="/Serie/id/:id" component={Serie}/>
-        <Route path="/Results" exact={true} component={Results}/>
-        <Route path="" component={NotFound}/>
-      </Switch>
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <Route exact path="/movies" component={Movies} />
+      <Route path="/movie/:id" component={Movie} />
+      <Route exact path="/series" component={Series} />
+      <Route path="/series/:id" component={Serie} />
+      <Route path="/results" component={Results} />
+
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
