4907b5c5514e3857f8ca152d1cb7429029f0a19d
Finnet97 <totochipont@gmail.com>
2025-09-18 13:32:35 -0300

    feat: search form en el header

--- trabajo-integrador/src/components/Header/Header.js
+++ trabajo-integrador/src/components/Header/Header.js
@@ -6,18 +6,22 @@ function Header() {
   return (
     <header className="site-header">
       <div className="brand">
-        <img src="/imagenes/BeWatch_logo.png" alt="" className="logo" />
+        <img src="/imagenes/BeWatch_logo.png" alt="BeWatch" className="logo" />
         <h1 className="brand-name">BeWatch</h1>
       </div>
+
+      <form className="search-form" action="/SearchResults" method="get" role="search" aria-label="Buscar en BeWatch">
+        <label className="sr-only">Buscar</label>
+        <input id="site-search" type="search" name="searchData" placeholder="Buscar..." className="search-input"/>
+      </form>
+
       <nav>
-         <ul className="main-nav">
+        <ul className="main-nav">
           <li><Link to="/">Home</Link></li>
-
           <li><Link to="/seeall/movie/popular">Películas populares</Link></li>
-          <li><Link to="/seeall/movie/now_playing">Películas en cartel</Link></li>
-
+          <li><Link to="/seeall/movie/now_playing">Cartelera</Link></li>
           <li><Link to="/seeall/tv/popular">Series populares</Link></li>
-          <li><Link to="/seeall/tv/airing_today">Series al aire hoy</Link></li>
+          <li><Link to="/seeall/tv/airing_today">Al aire</Link></li>
           <li><Link to="/favourites">Favoritos</Link></li>
         </ul>
       </nav>
