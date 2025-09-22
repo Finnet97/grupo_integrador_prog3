6c66854be16b1f6a6cb8eee4a2d867275dd9c8c6
odebuchy <odebuchy@udesa.edu.ar>
2025-09-22 12:48:04 -0300

    cambie los css de loader y not found

--- trabajo-integrador/src/screens/NotFound/NotFound.js
+++ trabajo-integrador/src/screens/NotFound/NotFound.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./notfound.css";
 
 export function NotFound() {
@@ -11,18 +10,6 @@ export function NotFound() {
         <p className="nf-text">
           Puede que el enlace esté roto o la ruta haya cambiado.
         </p>
-
-        <div className="nf-actions">
-          <Link className="btn-primary" to="/">Volver al inicio</Link>
-          <Link className="btn-ghost" to="/seeall/movie/popular">Películas populares</Link>
-          <Link className="btn-ghost" to="/seeall/tv/popular">Series populares</Link>
-        </div>
-
-        {/* Reciclado del otro buscador, cualquier cosa lo sacamos si es mucho quilombo (chipo) */}
-        <form className="search-form nf-search" action="/SearchResults" method="get" role="search" aria-label="Buscar en BeWatch">
-          <label className="sr-only">Buscar</label>
-          <input id="nf-search" type="search" name="searchData" placeholder="Probar con otra búsqueda…" className="search-input"/>
-        </form>
       </div>
 
       <div className="nf-decoration" aria-hidden="true"></div>
